f29e3a814c945a5572409a57e0cd02c3f88320e1
Mario Nguyễn <53334967+marionguyen20@users.noreply.github.com>
2020-09-15 11:35:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace TicketSelling
+{
+    static class Program
+    {
+       [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new fMain());
+        }
+    }
+}
